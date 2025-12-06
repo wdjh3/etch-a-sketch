9de9cb7718963f5d298abaae31cd4dc41cb6e34c
wdjh3 <198714742+wdjh3@users.noreply.github.com>
2025-12-06 12:52:16 +0800

    Add pixels turning black after mouse hover

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ let amountOfSquaresPerSide = 16;
 for ( let i = 0 ; i < amountOfSquaresPerSide * amountOfSquaresPerSide ; i++ ) {
     let square = document.createElement("div");
     square.classList.add("square");
-    
+
     container.appendChild(square);
 }
+
+container.addEventListener('mouseover',(event) => {
+    if( event.target.classList.contains("square") ) {
+        event.target.style.backgroundColor = "black";
+    };
+})
