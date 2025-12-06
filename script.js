568a94d9230dd1d734c0030539a2c588c03304a6
wdjh3 <198714742+wdjh3@users.noreply.github.com>
2025-12-06 15:58:59 +0800

    Refactor adding squares into a function

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
+const body = document.querySelector("body");
 const container = document.querySelector("#container");
 const button = document.querySelector("#btn");
 
-let amountOfSquaresPerSide = 16;
-for ( let i = 0 ; i < amountOfSquaresPerSide * amountOfSquaresPerSide ; i++ ) {
-    let square = document.createElement("div");
-    square.classList.add("square");
-
-    container.appendChild(square);
-}
+let numberOfSquaresPerSide = 16;
+addSquares(numberOfSquaresPerSide);
 
 container.addEventListener('mouseover',(event) => {
     if( event.target.classList.contains("square") ) {
@@ -15,3 +11,19 @@ container.addEventListener('mouseover',(event) => {
     };
 })
 
+btn.addEventListener('click', () => {
+    numberOfSquaresPerSide = prompt("Number of squares per side?");
+    // container.remove();
+    // let newContainer = document.createElement("div");
+    // newContainer.id = "container";
+    // body.appendChild(newContainer);
+})
+
+function addSquares(numberOfSquaresPerSide) {
+    for ( let i = 0 ; i < numberOfSquaresPerSide * numberOfSquaresPerSide ; i++ ) {
+        let square = document.createElement("div");
+        square.classList.add("square");
+
+        container.appendChild(square);
+    }
+}
